a40fa6b24fbdcb990f88c7c7e02437eb494397f7
MarionAule <marion.aule@gmail.com>
2019-03-28 09:13:21 +0200

    katki, avaleht pole ühendatud

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,38 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Controller {
+
+    @FXML
+    void switchScene(Stage stage) throws IOException {
+        Parent leht = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene aken = new Scene(leht);
+        stage.setScene(aken);
+        stage.show();
+    }
+
+    void MineTeiseleLehele(ActionEvent event) throws IOException {
+        Parent teineLeht = FXMLLoader.load(getClass().getResource("avaleht.fxml"));
+        Scene mänguLeht = new Scene(teineLeht);
+        Stage aken = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        aken.setScene(mänguLeht);
+        aken.show();
+    }
+
     @FXML
     ImageView pilt;
     Button nupp;
@@ -21,12 +45,14 @@ public class Controller {
     }
 
     @FXML
-    void määraPilt() throws FileNotFoundException {
+    void määraPilt() {
         pilt.setImage(null);
 
     }
+
     @FXML
-    void tiripilt(){
+    void tiripilt() {
         System.out.println("oki");
     }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,14 +10,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("avaleht.fxml"));
         primaryStage.setTitle("Asjad kokku kähku kähku");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
 
-    
+
     public static void main(String[] args) {
         launch(args);
     }
